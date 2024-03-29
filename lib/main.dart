6c4fccf806bb5460b5d57828b7b7e07a406d4c84
6cessfuldev <6cessfuldev@gmail.com>
2024-03-29 10:20:28 +0900

    ✨Feat(DragRouteCubit):드래그를 통한 화면 전환 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/routing/drag_route_cubit.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,115 +16,122 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+          create: (_) => DragRouteCubit(), child: const HomePage()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var screenSize = MediaQuery.of(context).size;
+
+    double secondBottomBarHeight = 80;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body:
+          BlocBuilder<DragRouteCubit, DragState>(builder: (context, dratState) {
+        return Stack(children: [
+          //가장 배경
+          Container(
+              color: Colors.white,
+              height: screenSize.height,
+              child: const Center(child: Text('HomePage'))),
+          Positioned(
+              bottom: 0,
+              child: GestureDetector(
+                onVerticalDragUpdate: ((details) => context
+                    .read<DragRouteCubit>()
+                    .handleDragUpdate(details.primaryDelta!)),
+                onVerticalDragEnd: ((details) =>
+                    context.read<DragRouteCubit>().handleDragEnd()),
+                child: Container(
+                  color: Colors.amber,
+                  height: 160 + (screenSize.height - 160) * dratState.scale,
+                  width: screenSize.width,
+                ),
+              )),
+          //상세 페이지의 바텀바
+          Positioned(
+              bottom: 0,
+              child: Opacity(
+                  opacity: dratState.scale,
+                  child: Container(
+                    color: Colors.green,
+                    height: secondBottomBarHeight * dratState.scale,
+                    width: screenSize.width,
+                  ))),
+          Positioned(
+              bottom: 0,
+              child: Opacity(
+                  opacity: 1 - dratState.scale,
+                  child:
+                      CustomBottomNaviBar(height: 80 * (1 - dratState.scale))))
+        ]);
+      }),
+    );
+  }
+}
+
+class CustomBottomNaviBar extends StatefulWidget {
+  const CustomBottomNaviBar({required this.height, super.key});
+  final double height;
+
+  @override
+  State<CustomBottomNaviBar> createState() => _CustomBottomNaviBarState();
+}
+
+class _CustomBottomNaviBarState extends State<CustomBottomNaviBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.red,
+        height: widget.height,
+        width: MediaQuery.of(context).size.width,
+        child: Image.asset('assets/images/user_profile_sample.jpg'));
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Container(
+        margin: const EdgeInsets.only(top: 70),
+        height: MediaQuery.of(context).size.height - 70,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: const Color.fromARGB(255, 255, 78, 84)),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white70,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
