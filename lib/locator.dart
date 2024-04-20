765c829ab98632549020bb6bff05972881b5864c
6cessfuldev <6cessfuldev@gmail.com>
2024-04-21 01:36:54 +0900

    ✨Feat(locator): 의존성 관리 파일 추가

---
+++ lib/locator.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+
+import 'domain/usecases/member/sign_in_usecase.dart';
+import 'domain/usecases/member/sign_out_usecase.dart';
+import 'domain/usecases/member/sign_up_usecase.dart';
+import 'presentation/blocs/member/member_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  //Features - User
+  // Bloc
+  sl.registerFactory(
+    () => MemberBloc(sl(), sl(), sl()),
+  );
+  // Use cases
+  sl.registerLazySingleton(() => SignInUseCase(sl()));
+  sl.registerLazySingleton(() => SignUpUseCase(sl()));
+  sl.registerLazySingleton(() => SignOutUseCase(sl()));
+}
