8035e80549811a0467212c5a9b6dc8d57b5bf655
6cessfuldev <6cessfuldev@gmail.com>
2024-05-01 22:29:32 +0900

    ✨Feat: ResponseModel<T>

---
+++ lib/data/models/response_model.dart
@@ -0,0 +1,30 @@
+import 'pagination/page.dart';
+
+class ResponseModel<T> {
+  final String status;
+  final T? data;
+  final Page? page;
+
+  ResponseModel({required this.status, required this.data, this.page});
+
+  factory ResponseModel.fromJsonList(
+      Map<String, dynamic> json, T Function(List<dynamic>) fromJson) {
+    return ResponseModel(
+        status: json["status"],
+        data: fromJson(json["data"]),
+        page: Page.fromJson(json["page"]));
+  }
+
+  factory ResponseModel.fromJsonMap(
+      Map<String, dynamic> json, T Function(Map<String, dynamic>) fromJson) {
+    return ResponseModel(
+        status: json["status"],
+        data: fromJson(json["data"]),
+        page: json["page"] == null ? null : Page.fromJson(json["page"]));
+  }
+
+  factory ResponseModel.fromJsonWithoutData(Map<String, dynamic> json) {
+    return ResponseModel(
+        status: json["status"], data: null, page: Page.fromJson(json["page"]));
+  }
+}
